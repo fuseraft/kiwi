ce08204d8b944227ff1a4d9aef0a9f86f8c4e56e
Scott Stauffer <scott@fuseraft.com>
2023-12-12 20:57:06 -0600

    WIP: better logging.

--- src/include/parsing/tokens.h
+++ src/include/parsing/tokens.h
@@ -5,45 +5,6 @@
 #include <string>
 #include "value_type.h"
 
-ValueType get_value_type(std::variant<int, double, bool, std::string> v) {
-    ValueType type = ValueType::None;
-    std::visit([&](auto&& arg) {
-        using T = std::decay_t<decltype(arg)>;
-
-        if constexpr (std::is_same_v<T, int>)
-            type = ValueType::Integer;
-        else if constexpr (std::is_same_v<T, double>)
-            type = ValueType::Double;
-        else if constexpr (std::is_same_v<T, bool>)
-            type = ValueType::Boolean;
-        else if constexpr (std::is_same_v<T, std::string>)
-            type = ValueType::String;
-        else
-            type = ValueType::Unknown;
-    }, v);
-    return type;
-}
-
-std::string get_value_type_string(ValueType vt) {
-    switch (vt) {
-        case ValueType::Boolean:
-            return "Boolean";
-        case ValueType::String:
-            return "String";
-        case ValueType::Integer:
-            return "Integer";
-        case ValueType::Double:
-            return "Double";
-        case ValueType::Unknown:
-            return "Unknown";
-        case ValueType::None:
-            return "None";
-    }
-    std::ostringstream error;
-    error << "Cannot determine value type `" << static_cast<int>(vt) << "`.";
-    throw std::runtime_error(error.str());
-}
-
 enum TokenType {
     IDENTIFIER,
     COMMENT,
@@ -58,14 +19,54 @@ enum TokenType {
     ENDOFFILE
 };
 
+std::string get_token_type_string(TokenType tt) {
+    switch (tt) {
+    case TokenType::IDENTIFIER:
+        return "IDENTIFIER";
+    case TokenType::COMMENT:
+        return "COMMENT";
+    case TokenType::KEYWORD:
+        return "KEYWORD";
+    case TokenType::OPERATOR:
+        return "OPERATOR";
+    case TokenType::LITERAL:
+        return "LITERAL";
+    case TokenType::STRING:
+        return "STRING";
+    case TokenType::NEWLINE:
+        return "NEWLINE";
+    case TokenType::ESCAPED:
+        return "ESCAPED";
+    case TokenType::OPEN_PAREN:
+        return "OPEN_PAREN";
+    case TokenType::CLOSE_PAREN:
+        return "CLOSE_PAREN";
+    case TokenType::ENDOFFILE:
+        return "ENDOFFILE";
+    }
+
+    std::ostringstream error;
+    error << "Cannot determine token type `" << static_cast<int>(tt) << "`.";
+    throw std::runtime_error(error.str());
+}
+
 struct Token {
     TokenType type;
     std::string text;
     std::variant<int, double, bool, std::string> value;
     ValueType value_type;
 
-    Token(TokenType t, std::string text, const std::variant<int, double, bool, std::string>& v) : type(t), text(text), value(v) {
+    Token(TokenType t, std::string text,
+          const std::variant<int, double, bool, std::string> &v)
+        : type(t), text(text), value(v) {
         value_type = get_value_type(v);
+    }
+
+    std::string info() {
+        std::ostringstream info;
+        info << "type: " << get_token_type_string(type) << ", "
+             << "text: " << text;
+        return info.str();
     }
 
     std::string toString() {
