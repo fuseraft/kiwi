594acfa5b3ae9be76d8d44e84d1febed2be21cb0
Scott Stauffer <scott@fuseraft.com>
2024-01-31 21:36:20 -0600

    Update comment header in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 /**
- *  kiwi: an unorthodox scripting language
- *  hybrid-typed, object-oriented, interpreted
- **/
+ * The Kiwi Programming Language
+ * =============================
+ * A general-purpose object-oriented scripting language influenced by Ruby.
+ *
+ * @author Scott Stauffer
+ * @date 2024-01-31
+ * @version 1.0.3
+ *
+ * GitHub Repository: https://github.com/fuseraft/kiwi
+ * For more information, issues, or to contribute, please visit the repository.
+ *
+ * License: MIT License
+ * SPDX-License-Identifier: MIT
+ * The full license text is available in the LICENSE file in the repository.
+ */
 
 #include <vector>
 #include "kiwi.h"
