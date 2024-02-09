9920629a299b0e66ada91ddc4ab418a12316de76
Scott Stauffer <scott@fuseraft.com>
2024-02-09 17:28:01 -0600

    Version bump to 1.0.10

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
  *
  * @author Scott Stauffer
  * @date 2024-01-31
- * @version 1.0.9
+ * @version 1.0.10
  *
  * GitHub Repository: https://github.com/fuseraft/kiwi
  * For more information, issues, or to contribute, please visit the repository.
