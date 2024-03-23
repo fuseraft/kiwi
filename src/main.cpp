fc0df285dcaa458608d9009cc1c34a3c6af8859a
Scott Stauffer <scott@fuseraft.com>
2024-03-23 12:39:34 -0500

    Version bump 1.3.3

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
  *
  * @author Scott Stauffer
  * @date 2024-01-31
- * @version 1.3.2
+ * @version 1.3.3
  *
  * GitHub Repository: https://github.com/fuseraft/astral
  * For more information, issues, or to contribute, please visit the repository.
