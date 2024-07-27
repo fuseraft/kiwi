0555c24c1a9c4e42f77d4a3b5120c45c6a99c9aa
Scott Stauffer <scott@fuseraft.com>
2024-07-27 10:34:26 -0500

    Cleanup

--- analyze.sh
+++ analyze.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p analyze
-cp -r include/* analyze
-cp src/main.cpp analyze
+cp -r kiwi/include/* analyze
+cp kiwi/src/main.cpp analyze
 cppcheck --enable=performance analyze
 rm -rf analyze
