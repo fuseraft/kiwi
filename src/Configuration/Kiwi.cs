ad9f0a6afb5e275927cf1be2dd802b47aa2f9fbe
Scott Stauffer <scott@fuseraft.com>
2026-01-15 23:44:42 -0600

    Version bump

--- src/Configuration/Kiwi.cs
+++ src/Configuration/Kiwi.cs
@@ -51,7 +51,7 @@ public class KiwiSettings
         return new KiwiSettings
         {
             Name = "kiwi",
-            Version = "1.3.3",
+            Version = "1.3.4",
             SafeMode = true,
             Extensions = new FileExtensions
             {
